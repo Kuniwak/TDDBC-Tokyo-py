a50191055501167f2a4dd6867e9d11ac57745909
Kuniwak <yuki.kokubun@mixi.co.jp>
2014-11-22 15:55:59 +0900

    Refactor remove_task_by_index

--- task_collection.py
+++ task_collection.py
@@ -8,11 +8,20 @@ class TaskCollection(object):
     def append_task(self, task):
         self.task_list.append(task)
 
+    def is_available_index(self, index):
+        return len(self.task_list) > index >= 0
+
     def get_last_task(self):
         return self.get_task_by_index(len(self.task_list) - 1)
 
     def get_task_by_index(self, index):
-        if len(self.task_list) > index >= 0:
-            return self.task_list[index]
-        else:
+        if not self.is_available_index(index):
             return None
+
+        return self.task_list[index]
+
+    def remove_task_by_index(self, index):
+        if not self.is_available_index(index):
+            return
+
+        self.task_list.pop(index)
--- test_task_collection.py
+++ test_task_collection.py
@@ -4,6 +4,17 @@ import unittest
 
 
 class TestTaskCollection(unittest.TestCase):
+    def create_task_collection_has_a_task(self, opt_task=None):
+        task_collection = TaskCollection()
+
+        if opt_task:
+            task = opt_task
+        else:
+            task = Task('summary', 'description')
+
+        task_collection.append_task(task)
+        return task_collection
+
     def test_create_task_collection(self):
         self.assertIsInstance(TaskCollection(), TaskCollection)
 
@@ -12,8 +23,7 @@ class TestTaskCollection(unittest.TestCase):
         self.assertTrue(task_collection.is_empty())
 
     def test_append_task(self):
-        task_collection = TaskCollection()
-        task_collection.append_task(Task('summary', 'description'))
+        task_collection = self.create_task_collection_has_a_task()
         self.assertFalse(task_collection.is_empty())
 
     def test_get_last_task(self):
@@ -25,10 +35,31 @@ class TestTaskCollection(unittest.TestCase):
         self.assertEqual(task_collection.get_task_by_index(0), None)
 
     def test_get_task_by_index_with_existent_index(self):
-        task_collection = TaskCollection()
         task = Task('summary', 'desc')
-        task_collection.append_task(task)
+        task_collection = self.create_task_collection_has_a_task(task)
+
         self.assertEqual(task_collection.get_task_by_index(0), task)
+
+    def test_remove_task_by_index(self):
+        task_collection = self.create_task_collection_has_a_task()
+
+        task_collection.remove_task_by_index(0)
+        self.assertTrue(task_collection.is_empty())
+
+    def test_remove_task_by_index_with_unexistent_index(self):
+        task_collection = self.create_task_collection_has_a_task()
+
+        task_collection.remove_task_by_index(1)
+        self.assertFalse(task_collection.is_empty())
+
+    def test_is_available_index_with_unexistent_index(self):
+        task_collection = TaskCollection()
+        self.assertFalse(task_collection.is_available_index(0))
+
+    def test_is_available_index_with_existent_index(self):
+        task_collection = self.create_task_collection_has_a_task()
+        self.assertTrue(task_collection.is_available_index(0))
+
 
 
 if __name__ == '__main__':
