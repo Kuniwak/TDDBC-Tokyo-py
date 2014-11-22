89aef7ac13c97a314321172d343a810230245759
Kuniwak <yuki.kokubun@mixi.co.jp>
2014-11-22 13:45:09 +0900

    Add test skelton

---
+++ test/test_task.py
@@ -0,0 +1,13 @@
+import unittest
+# import Task
+
+
+class TestTask(unittest.TestCase):
+    def test_create_task(self):
+        pass
+        # task = Task()
+        # self.assertIsInstance(task, Task)
+
+
+if __name__ == '__main__':
+    unittest.main()
